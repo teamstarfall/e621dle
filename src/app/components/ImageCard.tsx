b73569bdc1e98183f8b04142dbd470353ba0a1e2
angelolz <angel.lugia98@gmail.com>
2025-09-08 03:36:09 -0600

    added image loading logic

--- src/app/components/ImageCard.tsx
+++ src/app/components/ImageCard.tsx
@@ -1,30 +1,45 @@
 "use client";
 
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useMemo, useState } from "react";
 import Image from "next/image";
 import { Tag } from "../interfaces";
 
 const ImageCard = memo(function ImageCard({ tag }: { tag: Tag }) {
-    const [src, setSrc] = useState(tag.images.safe.url);
+    const sources = useMemo(() => {
+        const potential = [];
+        const ratings: (keyof Tag["images"])[] = ["safe", "questionable"];
+
+        for (const rating of ratings) {
+            const url = tag.images[rating]?.url;
+            if (url) {
+                potential.push(url);
+                potential.push(url.replace("/sample/", "/"));
+            }
+        }
+        return potential;
+    }, [tag]);
+
+    const [sourceIndex, setSourceIndex] = useState(0);
 
     useEffect(() => {
-        // Reset src when the tag changes
-        setSrc(tag.images.safe.url);
+        setSourceIndex(0);
     }, [tag]);
 
     const handleError = () => {
-        if (src === tag.images.safe.url) {
-            setSrc(tag.images.questionable.url);
-        } else {
-            setSrc(null);
+        if (sourceIndex + 1 < sources.length) {
+            console.log(`Failed to load image: ${sources[sourceIndex]}, currently trying: ${sources[sourceIndex + 1]}`);
+            setSourceIndex((prevIndex) => prevIndex + 1);
         }
     };
 
+    const currentSrc = sources[sourceIndex];
+
     return (
         <>
-            {src && (
+            {currentSrc && (
                 <Image
-                    src={src}
+                    key={currentSrc}
+                    src={currentSrc}
                     alt={tag.name}
                     fill
                     className="object-contain rounded-md"
