e2352ac8d37eb2181962654becf292b2138f2275
angelolz <angel.lugia98@gmail.com>
2025-09-13 22:42:21 -0600

    remove unecessary stuff

--- src/app/components/Spinner.tsx
+++
@@ -1,5 +0,0 @@
-export default function Spinner() {
-    return (
-        <div className="animate-spin rounded-full w-8 h-8 border-white border-4 border-b-transparent"></div>
-    );
-}
--- src/app/constants.ts
+++
@@ -1,8 +0,0 @@
-export const SHOW_ANSWER_TIME_MS = 5000;
-export const INCREMENT_ANIM_MS = 3500;
-export const OPACITY_ANIM_MS = 1000;
-export const SETTINGS_RATING_LEVEL = "ratingLevel";
-export const SETTINGS_CHARACTER_TAGS_ONLY = "characterTagsOnly";
-export const SETTINGS_ADULT_WARNING = "adultWarningAccept";
-export const SETTINGS_PAUSE_BEFORE_NEXT = "pauseBeforeNext";
-export const BEST_STREAK = "bestStreak";
