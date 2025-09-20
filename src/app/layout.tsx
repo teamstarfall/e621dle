be6c1f6c702190ef63e18781ea1e6d0c950d406c
angelolz <angel.lugia98@gmail.com>
2025-09-20 05:57:47 -0600

    adding some metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "e621dle",
-    description: "Guess which tag has the most posts!",
+    description: "A game where you guess which furry tag has the most posts!",
+    applicationName: "e621dle",
+    keywords: "e621dle, e621, furry, wordle, rule34dle, -dle games, game",
+    openGraph: {
+        type: "website",
+        url: "https://e621dle.starfall.team",
+        title: "e621dle",
+        description: "A game where you guess which furry tag has the most posts!",
+        siteName: "e621dle",
+        images: [{ url: "https://e621dle.starfall.team/preview.png" }],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "e621dle by Team Starfall",
+        description: "A game where you guess which furry tag has the most posts!",
+        images: [{ url: "https://e621dle.starfall.team/preview.png" }],
+    },
 };
 
 export default function RootLayout({
