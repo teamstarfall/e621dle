3a64e04012a696f6591751bc4463a0f1173cdfc4
angelolz <angel.lugia98@gmail.com>
2026-06-23 19:35:51 -0600

    added socials to error page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,36 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 function Error() {
-    return <div className="w-screen h-screen flex items-center justify-center">Something went wrong.</div>;
+    return (
+        <div className="w-screen h-screen flex flex-col items-center justify-center gap-2 text-center">
+            <span>Something went wrong.</span>
+            <span className="text-sm text-neutral-500">
+                Reach out on{" "}
+                <a
+                    className="underline"
+                    href="http://github.com/teamstarfall/e621dle"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    GitHub
+                </a>
+                {", "}
+                <a className="underline" href="http://twitter.com/angelolz1" target="_blank" rel="noopener noreferrer">
+                    Twitter
+                </a>
+                {", or "}
+                <a
+                    className="underline"
+                    href="https://bsky.app/profile/angelolz.one"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Bluesky
+                </a>
+                .
+            </span>
+        </div>
+    );
 }
 
 function Loading() {
